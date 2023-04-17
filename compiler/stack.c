7621a4765f32de89c9fc412787e8be83f076c793
leoniegtt <leonie.goutte@gmail.com>
2023-04-17 09:14:10 +0200

    stack ok - tests ok

--- compiler/stack.c
+++ compiler/stack.c
@@ -16,6 +16,7 @@ typedef struct stack
 
 void stack_push (stack_s ** pp_stack, char* id, int address , int profondeur)
 {
+
    if (pp_stack != NULL)
    {
 // insert element after p_p pointer
@@ -36,8 +37,6 @@ void stack_push (stack_s ** pp_stack, char* id, int address , int profondeur)
             p_p->next = p_l;
 // point pp_pile on new element
          *pp_stack = p_l;
-         int addr = p_p->address;
-         printf("-----------------\n");
       }
       else
       {
@@ -69,27 +68,32 @@ void stack_pop (stack_s ** pp_stack)
 void profondeur_pop (stack_s ** pp_stack)
 {
 
-    stack_s *p_p = *pp_stack;
-    int profondeur_actu = p_p->profondeur;
-
-    //while(p_p->)
-    while ((*p_p).profondeur == profondeur_actu){
-        stack_pop(pp_stack);
-        stack_s *p_p = *pp_stack;
-    }
+    
+   int profondeur_actu = (*pp_stack)->profondeur;
+   printf("la prof est %d \n",profondeur_actu);
+   while ((*pp_stack)->profondeur == profondeur_actu){   
+      stack_pop(pp_stack);  
+   }
 }
 
-/*stack_s* get_top(stack_s ** pp_stack)
+ int est_prest(stack_s ** pp_stack,char* id)
 {
-     stack_s *p_p = *pp_stack;
-     stack_s *top = p_p;
-     while(top->next!=NULL){
-        top=top->next;
-     }
-     return top;
-
+   int est_pres=0;
+   if (pp_stack != NULL && *pp_stack != NULL)
+   {  
+      stack_s *pp = *pp_stack; 
+      while (pp != NULL){
+         if( strcmp(((pp)->id), id) == 0 ){
+            est_pres=1;
+            break;
+         }
+         pp = (pp)->prev;
+      }
+      return est_pres;
+   }
+   
 }
-*/
+
 
 void stack_delete (stack_s ** pp_stack)
 {
@@ -101,7 +105,31 @@ void stack_delete (stack_s ** pp_stack)
 
 }
 
+void printstack (stack_s ** pp_stack){
+   if (pp_stack != NULL && *pp_stack != NULL)
+   {
+   stack_s *pp = *pp_stack; 
+      while ((pp)->prev != NULL){
+         printf("----------------------- %d ----------- \n",(pp)->address);
+         pp = (pp)->prev;
+         }
+      printf("----------------------- %d ----------- \n",(pp)->address);
+   }
+
+}
 int main(){
+   /* tests de la pile*/
     stack_s* mapile = NULL;
     stack_push(&mapile , "toto",  5050 ,  0);
+    stack_push(&mapile , "titi",  5055 ,  0);
+    stack_push(&mapile , "tata",  5060 ,  1);
+    stack_push(&mapile , "tt",  5065 ,  1);
+    stack_push(&mapile , "ttd",  5070 ,  1);
+    printstack(&mapile);
+    printf("____________________________________________________ \n");
+    profondeur_pop(&mapile);
+    printstack(&mapile);
+    printf("%d \n",est_prest(&mapile , "tito"));
+
+
 }
